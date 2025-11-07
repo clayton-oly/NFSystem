f6ddd61f5f27cdba6bf3389c91bc4f691f3bc45d
Clayton Oliveira <clayton.will08@gmail.com>
2025-11-07 03:01:19 -0300

    Adicionar arquivos de projeto.

---
+++ NFSystem/Models/NotaFiscal.cs
@@ -0,0 +1,10 @@
+﻿namespace NFSystem.Models
+{
+    public class NotaFiscal
+    {
+        public int Id { get; set; }
+        public int NumeroSequencial { get; set; }
+        public string Status { get; set; } = "Aberta";
+
+    }
+}
---
+++ NFSystem/Models/Produto.cs
@@ -0,0 +1,10 @@
+﻿namespace NFSystem.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public int Codigo { get; set; }
+        public string Descricao { get; set; }
+        public int Saldo { get; set; }
+    }
+}
